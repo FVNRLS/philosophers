ec43adab3d62cbcbe0735f38d6acdde31c208252
Roman Mazuritskiy <rmazurit@2-E-2.42heilbronn.de>
2022-08-13 14:02:15 +0200

    init phils

--- philo/src/destructors/forks_destr.c
+++ philo/src/destructors/forks_destr.c
@@ -1,14 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   forks_initializer.c                                :+:      :+:    :+:   */
+/*   forks_manager.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmazurit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 15:38:06 by rmazurit          #+#    #+#             */
-/*   Updated: 2022/08/12 16:30:35 by rmazurit         ###   ########.fr       */
+/*   Updated: 2022/08/13 12:12:56 by rmazurit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/philo.h"
 
+
+
+static void	init_left_fork(t_phil *phil)
+{
+
+}
+
+static void	init_right_fork(t_phil *phil)
+{
+
+}
+
+void	init_forks(t_phil *phil)
+{
+
+}
---
+++ philo/src/initializers/forks_init.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   forks_initializer.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmazurit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 15:38:06 by rmazurit          #+#    #+#             */
+/*   Updated: 2022/08/13 12:08:22 by rmazurit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/philo.h"
+
+
+
+static void	init_left_fork(t_phil *phil)
+{
+
+}
+
+static void	init_right_fork(t_phil *phil)
+{
+
+}
+
+void	init_forks(t_phil *phil)
+{
+
+}
